127290234fc1870be0f604c52bae88711a4dc572
Abdoul Goudoussy Diallo[OGN DSI/DDF/SAI] <DIALLO110631@orange-sonatel.com>
2022-06-13 18:13:56 +0200

    inital commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>I am a tittle</h1>
+      <h2>I am a tittle</h2>
+      <h3>I am a tittle</h3>
+      <h4>I am a tittle</h4>
+      <h5>I am a tittle</h5>
+      <a href="">jklsjdfklmsjmlfk</a>
+      <button>klsdfj</button>
+      <ul>
+        <li></li>
+      </ul>
+    </div>
+  );
+}
+
+export default Home;
